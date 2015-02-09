fd7682513f9f6413fb5229c68f79512f4be79cc3
HakanBergman <hakan.bergman@drumedar.se>
2015-02-09 02:32:58 +0100

    Master

--- htdocs/ezpublish_legacy/design/newscenter/javascript/dropdown.js
+++ htdocs/ezpublish_legacy/design/newscenter/javascript/dropdown.js
@@ -2,12 +2,11 @@
     $('a.disabled').click(function () {
 
         /* Hide all submenus */       
-        $('.submenu').stop().animate({ opacity: 0 }, "slow");
         $('.submenu').slideUp('slow');
 
         /* Show this submenu */
         $('#' + $(this).attr('data-dropdown')).stop().animate({ opacity: 1 }, "slow");
-        $('#' + $(this).attr('data-dropdown')).slideDown('fast');
+        $('#' + $(this).attr('data-dropdown')).slideDown('slow');
 
         /* Return false */
         return false;
