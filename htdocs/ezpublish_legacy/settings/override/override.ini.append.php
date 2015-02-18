8316bc486c2256ccfd9f0b5be56f9b147572216b
HakanBergman <hakan.bergman@drumedar.se>
2015-02-18 13:00:26 +0100

    Master

--- htdocs/ezpublish_legacy/settings/override/override.ini.append.php
+++ htdocs/ezpublish_legacy/settings/override/override.ini.append.php
@@ -5,5 +5,12 @@ Source=node/view/full.tpl
 MatchFile=full/product_catalogue.tpl
 Subdir=templates
 Match[class_identifier]=product_catalogue
+
+[full_product_category]
+Source=node/view/full.tpl
+MatchFile=full/product_category.tpl
+Subdir=templates
+Match[class_identifier]=product_category
+
       
 */ ?>
