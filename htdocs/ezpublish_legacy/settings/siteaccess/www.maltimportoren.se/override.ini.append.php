5e601cec8021406b55a8a76d050cc5c5597693fb
HakanBergman <hakan.bergman@drumedar.se>
2015-02-18 12:46:04 +0100

    Master

--- htdocs/ezpublish_legacy/settings/siteaccess/www.maltimportoren.se/override.ini.append.php
+++ htdocs/ezpublish_legacy/settings/siteaccess/www.maltimportoren.se/override.ini.append.php
@@ -1,11 +1,5 @@
 <?php /* #?ini charset="utf-8"?
 
-[full_product_catalogue]
-Source=node/view/full.tpl
-MatchFile=full/product_catalogue.tpl
-Subdir=templates
-Match[class_identifier]=product_catalogue
-
 [block_campaign]
 Source=block/view/view.tpl
 MatchFile=block/campaign.tpl
