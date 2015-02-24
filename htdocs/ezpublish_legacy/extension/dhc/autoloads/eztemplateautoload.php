2a2883db764d44d98341f640108233cae57979f4
HakanBergman <hakan.bergman@drumedar.se>
2015-02-24 10:59:48 +0100

    Master

---
+++ htdocs/ezpublish_legacy/extension/dhc/autoloads/eztemplateautoload.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Look in the operator files for documentation on use and parameters definition.
+ *
+ * @var array $eZTemplateOperatorArray
+ */
+$eZTemplateOperatorArray = array();
+
+
+?>
