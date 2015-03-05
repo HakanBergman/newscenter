a5465c28850016ce41d9920229eefe25eb461800
HakanBergman <hakan.bergman@drumedar.se>
2015-03-05 16:23:06 +0100

    Master

--- htdocs/ezpublish_legacy/extension/dhc/autoloads/ezcreatecaptcha.php
+++ htdocs/ezpublish_legacy/extension/dhc/autoloads/ezcreatecaptcha.php
@@ -50,13 +50,6 @@ class eZCreateCaptcha
         {
             case 'ezcreatecaptcha':
             {
-            header ('Content-Type: image/png');
-$im = @imagecreatetruecolor(120, 20)
-      or die('Cannot Initialize new GD image stream');
-$text_color = imagecolorallocate($im, 233, 14, 91);
-imagestring($im, 1, 5, 5,  'A Simple Text String', $text_color);
-imagepng($im);
-imagedestroy($im);
                 /* Verify if the captcha already exists */
                 if (empty($_SESSION["dhc"]["captcha"])) {
                     /* Create the Captcha */
