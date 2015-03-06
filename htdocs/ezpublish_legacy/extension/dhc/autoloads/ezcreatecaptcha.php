db994eb6e38c4782f971dd51013def9b14bce111
HakanBergman <hakan.bergman@drumedar.se>
2015-03-06 18:18:51 +0100

    Master

--- htdocs/ezpublish_legacy/extension/dhc/autoloads/ezcreatecaptcha.php
+++ htdocs/ezpublish_legacy/extension/dhc/autoloads/ezcreatecaptcha.php
@@ -53,7 +53,7 @@ class eZCreateCaptcha
                 /* Create the Captcha */
                 $md5_hash = md5(rand(0,999)); 
                 $security_code = substr($md5_hash, 15, 8);
-                $image = shell_exec('echo '.$security_code.'|convert -channel RGBA -density 196 -resample 72 -bordercolor none -background none -pointsize 18 text:- -fill blue /var/www/newscenter/htdocs/ezpublish_legacy/design/newscenter/images/testdir/ps_transparent.png 2>&1');
+                $image = shell_exec('echo '.$security_code.'|convert -channel RGBA -density 196 -resample 72 -bordercolor none -background none -pointsize 18 text:- -fill black /var/www/newscenter/htdocs/ezpublish_legacy/design/newscenter/images/testdir/ps_transparent.png 2>&1');
                 echo $image;
                 if (empty($_SESSION["dhc"]["captcha"])) {                                
                     $_SESSION["dhc"]["captcha"] = $security_code;
