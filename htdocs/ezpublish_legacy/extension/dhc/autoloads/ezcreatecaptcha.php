c9517a07648759cb64d719c3bc0b6dc7abd9d731
HakanBergman <hakan.bergman@drumedar.se>
2015-03-06 20:02:37 +0100

    Master

--- htdocs/ezpublish_legacy/extension/dhc/autoloads/ezcreatecaptcha.php
+++ htdocs/ezpublish_legacy/extension/dhc/autoloads/ezcreatecaptcha.php
@@ -56,10 +56,10 @@ class eZCreateCaptcha
                 
                 /* Verify if we already have a captcha set in our cookie */
                 if (empty($_COOKIE["captcha"])) {  
-                    setcookie("captcha",  $security_code, time()+3600);
+                    setcookie("captcha",  $security_code, time()+60);
                     /* Don't create the image if it already exists */
                     if (!file_exists($_SERVER["DOCUMENT_ROOT"].'/design/newscenter/images/captcha/'.$security_code.'.png')) {
-                        $create_image = shell_exec('echo '.$security_code.'|convert -size 170x75 -channel RGBA -density 144 -bordercolor none -background none -pointsize 300 text:- -fill black '.$_SERVER["DOCUMENT_ROOT"].'/design/newscenter/images/captcha/'.$security_code.'.png 2>&1');
+                        $create_image = shell_exec('echo '.$security_code.'|convert -size 170x75 -channel RGBA -density 90 -bordercolor none -background none -pointsize 100 text:- -fill black '.$_SERVER["DOCUMENT_ROOT"].'/design/newscenter/images/captcha/'.$security_code.'.png 2>&1');
                     }                    
                     $operatorValue = $_COOKIE["captcha"];
                 } else {
