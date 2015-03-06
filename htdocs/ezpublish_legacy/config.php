90a2a70c32b47f9a28093ac0c13404c39ee93843
HakanBergman <hakan.bergman@drumedar.se>
2015-03-06 12:50:29 +0100

    Master

--- htdocs/ezpublish_legacy/config.php
+++ htdocs/ezpublish_legacy/config.php
@@ -1,5 +1,6 @@
 <?php
-    date_default_timezone_set( 'Europe/Stockholm' );
+    date_default_timezone_set("Europe/Stockholm");
+    echo date("Y/m/d H:i:s");
     ini_set('display_startup_errors',1);
     ini_set('display_errors',1);
     error_reporting(-1);
