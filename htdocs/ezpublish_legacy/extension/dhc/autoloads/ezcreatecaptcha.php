c0d2b9a15322257376e2d6f25f2e6602a6220764
HakanBergman <hakan.bergman@drumedar.se>
2015-03-09 13:32:12 +0100

    Master

--- htdocs/ezpublish_legacy/extension/dhc/autoloads/ezcreatecaptcha.php
+++ htdocs/ezpublish_legacy/extension/dhc/autoloads/ezcreatecaptcha.php
@@ -67,10 +67,9 @@ class eZCreateCaptcha
                     setcookie("captcha",  $security_code, time()+60);
                     /* Don't create the image if it already exists */
                     $create_image = shell_exec('convert -resize 300x25! -font Bookman-Demi -channel RGBA -density 100 -bordercolor none -background none -pointsize 72 label:'.$security_code.' -fill black '.$_SERVER["DOCUMENT_ROOT"].'/design/newscenter/images/captcha/'.$security_code.'.png 2>&1');
-                    echo "hit";
+                    var_dump($_COOKIE);
                     $operatorValue = $_COOKIE["captcha"];
                 } else {
-                    var_dump($_COOKIE);
                     $operatorValue = $_COOKIE["captcha"];                
                 }
                 
