500ec73fddd51a38b19552ca0f89d6eff8a5a76a
HakanBergman <hakan.bergman@drumedar.se>
2015-03-10 12:37:50 +0100

    Master

--- htdocs/ezpublish_legacy/settings/siteaccess/ezdemo_site_clean_admin/contentstructuremenu.ini.append.php
+++ htdocs/ezpublish_legacy/settings/siteaccess/ezdemo_site_clean_admin/contentstructuremenu.ini.append.php
@@ -12,4 +12,21 @@ ShowClasses[]=forums
 ShowClasses[]=gallery
 ShowClasses[]=blog
 ShowClasses[]=place_list
+ShowClasses[]=company_group
+ShowClasses[]=company_name
+ShowClasses[]=user
+ShowClasses[]=zone
+ShowClasses[]=zone_footer
+ShowClasses[]=website
+ShowClasses[]=product_list
+ShowClasses[]=product_category
+ShowClasses[]=product_catalogue
+ShowClasses[]=product
+ShowClasses[]=news_list
+ShowClasses[]=news_category
+ShowClasses[]=article
+ShowClasses[]=contact_list
+ShowClasses[]=contact_region
+ShowClasses[]=contact
+
 */ ?>
