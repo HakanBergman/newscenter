7c35d94fea588eda230bc722cc7ad561aa7f9b94
HakanBergman <hakan.bergman@drumedar.se>
2015-03-10 16:59:25 +0100

    Master

--- htdocs/ezpublish_legacy/design/newscenter/javascript/responsive.js
+++ htdocs/ezpublish_legacy/design/newscenter/javascript/responsive.js
@@ -4,7 +4,7 @@
 
     function resize() {
         if ($window.width() < 1450) {
-            return $('#sidebar_admin').removeClass('absolute');
+            return $('#sidebar_admin').removeClass('absolute').removeClass('col-lg-2').addClass('col-lg-12');
         }
 
         $html.removeClass('mobile');
