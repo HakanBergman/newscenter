85c82b6bcb7aeeb9e0bfd29b5d04184e2885205d
HakanBergman <hakan.bergman@drumedar.se>
2015-03-11 14:42:43 +0100

    Master

--- htdocs/ezpublish_legacy/design/newscenter/javascript/admin_bar.js
+++ htdocs/ezpublish_legacy/design/newscenter/javascript/admin_bar.js
@@ -1,2 +1,3 @@
 ﻿/* Make the Menu bar toggle */
 $('.toggle-admin-bar').click(function () { $('.admin-bar').toggle(); });
+$('.toggle-admin-bar').click(function () { $('.admin-bar').removeClass('hide'); });
