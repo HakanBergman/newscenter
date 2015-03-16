ded259e110fbfee92f46e1660a27981dfe82bdf8
HakanBergman <hakan.bergman@drumedar.se>
2015-03-16 17:11:10 +0100

    Master

--- htdocs/ezpublish_legacy/design/newscenter/javascript/overlay.js
+++ htdocs/ezpublish_legacy/design/newscenter/javascript/overlay.js
@@ -1,10 +1,6 @@
 ﻿$(document).ready(function () {
     $('.overlay').each(function () {
         console.log($(this).attr('id'));
-        if (jQuery.data(document.body, "overlay").value) {
-            console.log('value');
-        } else {
-            console.log('no value');
-        }
+        console.log($.cookies.get());
     });
 });
