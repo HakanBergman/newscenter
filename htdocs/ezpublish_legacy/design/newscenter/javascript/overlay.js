99ec639248925f4df6ce89ff08c95bb3af50501d
HakanBergman <hakan.bergman@drumedar.se>
2015-03-16 17:42:47 +0100

    Master

--- htdocs/ezpublish_legacy/design/newscenter/javascript/overlay.js
+++ htdocs/ezpublish_legacy/design/newscenter/javascript/overlay.js
@@ -5,7 +5,7 @@
             console.log('cookie exists');
         } else {
             $('#' + $(this).attr('id')).removeClass('hide');
-            $('#')
+            $('#overlay-content-' + $(this).attr('data-src')).removeClass('hide');
             console.log('creating cookie');
         }
     });
