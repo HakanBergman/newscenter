1ccec1b61b6800b657c9e0d672d91f9b80aed781
HakanBergman <hakan.bergman@drumedar.se>
2015-03-23 15:09:40 +0100

    Master

--- htdocs/ezpublish_legacy/design/newscenter/javascript/toggle_class.js
+++ htdocs/ezpublish_legacy/design/newscenter/javascript/toggle_class.js
@@ -1,12 +1,6 @@
-YUI(YUI3_config).use('event', function (Y) {
-    Y.on('domready', function () {
-        Y.all('*[data-action=toggleclass]').each(function(elem) {
-            var cl = elem.getAttribute('data-class'),
-                targets = Y.all(elem.getAttribute('data-target'));
-            elem.on('click', function (e) {
-                e.preventDefault();
-                targets.toggleClass(cl);
-            });
-        });
+$(document).ready(function () {
+    $('.toggle').on('click', function (e) {
+        e.preventDefault();
+        alert($(this).attr('data-href'));
     });
 });
--- htdocs/ezpublish_legacy/design/newscenter/override/templates/edit/sitesettings.tpl
+++ htdocs/ezpublish_legacy/design/newscenter/override/templates/edit/sitesettings.tpl
@@ -13,7 +13,7 @@
 
                 <div class="col-lg-12 no-margin no-padding">
                   <div class="pull-left"><h3 class="clear">SEO - Inställningar</h3></div>
-                  <div class="pull-right"><a class="btn toggle" data-href="seo" href="#seo">Visa detaljer</a></div>
+                  <div class="pull-right"><a class="toggle" data-href="seo" href="#seo">Visa detaljer</a></div>
                 </div>
                 
                 <p>Ställ in beskrivning och de sökord som du vill ska komma upp i organiska resultatet (Google, Bing, med flera).</p>
