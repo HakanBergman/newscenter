ec9c67829ce0d4206b539b2649e416544f8a6834
HakanBergman <hakan.bergman@drumedar.se>
2015-03-25 11:51:09 +0100

    Master

--- htdocs/ezpublish_legacy/design/newscenter/javascript/color_picker.js
+++ htdocs/ezpublish_legacy/design/newscenter/javascript/color_picker.js
@@ -1,5 +1,10 @@
-﻿$(document).ready(function () {
-    if ($('.picked-color').length) { alert('Found with Length'); }
+﻿$(document).ready(function(e) {
+    /* Check if we are in edit mode and can pick a color */
+    if ($('.picked-color').length) {
+        $('.picked-color .color').html('test');
+        $('.picked-color').removeClass('hide');
+    }
+    /* Make something happen when we click a color */
     $('.colors .color').on('click', function (e) {
         alert('du klickade på en färg');
     });
