2904b21feb11d2fa4238859e296b0d4c826fa754
HakanBergman <hakan.bergman@drumedar.se>
2015-03-25 12:02:32 +0100

    Master

--- htdocs/ezpublish_legacy/design/newscenter/javascript/color_picker.js
+++ htdocs/ezpublish_legacy/design/newscenter/javascript/color_picker.js
@@ -1,12 +1,12 @@
 ﻿$(document).ready(function(e) {
     /* Check if we are in edit mode and can pick a color */
     if ($('.background-picked-color').length) {        
-        $('.background-picked-color .color').html($('.background-saved-color').val());
+        $('.background-picked-color .color').html($('.background-saved-color input').val());
         $('.background-picked-color').removeClass('hide');
     }
     /* Make something happen when we click a color */
     $('.colors .background-color').on('click', function (e) {
         $('.background-picked-color .hex').html($(this).attr('data-hex'));
-        $('.background-saved-color').html($(this).attr('data-color'));
+        $('.background-saved-color input').val($(this).attr('data-color'));
     });
 });
