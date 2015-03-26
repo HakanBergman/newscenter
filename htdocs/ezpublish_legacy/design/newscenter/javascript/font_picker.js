b8d59c3006062074ba6a1cdc8395be090931ea8f
HakanBergman <hakan.bergman@drumedar.se>
2015-03-26 14:58:16 +0100

    Master

--- htdocs/ezpublish_legacy/design/newscenter/javascript/font_picker.js
+++ htdocs/ezpublish_legacy/design/newscenter/javascript/font_picker.js
@@ -6,6 +6,7 @@
     $('.fonts .font-size').on('click', function (e) {
         e.preventDefault();
         var font_size = classtosize($(this).attr('data-class'));
+        alert(font_size);
     });
 });
 
