2293dda41e4608ff92e605871af02f9da86ce143
HakanBergman <hakan.bergman@drumedar.se>
2015-03-26 16:28:16 +0100

    Master

--- htdocs/ezpublish_legacy/design/newscenter/javascript/font_picker.js
+++ htdocs/ezpublish_legacy/design/newscenter/javascript/font_picker.js
@@ -19,7 +19,7 @@
         var font_family = classtofont($(this).attr('data-class'));
         var font_family_class = $(this).attr('data-class');
         $('.font-family-picked').html(font_family);
-        $('.font_family').val(font_family_class);
+        $('.font-family-saved input').val(font_family_class);
         //alert($(this).attr('data-class'));
     });
 });
