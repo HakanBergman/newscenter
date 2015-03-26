f4c277d932139434b5b0a67b6fe3040634b5c112
HakanBergman <hakan.bergman@drumedar.se>
2015-03-26 14:57:41 +0100

    Master

--- htdocs/ezpublish_legacy/design/newscenter/javascript/font_picker.js
+++ htdocs/ezpublish_legacy/design/newscenter/javascript/font_picker.js
@@ -5,6 +5,29 @@
     }
     $('.fonts .font-size').on('click', function (e) {
         e.preventDefault();
-        alert($(this).attr('data-class'));
+        var font_size = classtosize($(this).attr('data-class'));
     });
 });
+
+function classtosize(input_class) {
+    switch (input_class) {
+        case "font-size-24-px":
+            return "24 pixlar";
+        break;
+        case "font-size-22-px":
+            return "22 pixlar";
+        break;
+        case "font-size-20-px":
+            return "20 pixlar";
+        break;
+        case "font-size-18-px":
+            return "18 pixlar";
+        break;
+        case "font-size-16-px":
+            return "16 pixlar";
+        break;
+        case "font-size-14-px":
+            return "14 pixlar";
+        break;
+    }
+}
