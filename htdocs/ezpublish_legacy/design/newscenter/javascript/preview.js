6b24aad7ae3bb9b51d79f463babd58da0bb1edff
HakanBergman <hakan.bergman@drumedar.se>
2015-04-07 13:59:01 +0200

    Master

--- htdocs/ezpublish_legacy/design/newscenter/javascript/preview.js
+++ htdocs/ezpublish_legacy/design/newscenter/javascript/preview.js
@@ -1,7 +1,7 @@
 $(document).ready(function (e) {
     $('.toggle-preview').on('click', function () {
-        if ($.cookie('preview')) {
-            alert('preview är nu av');
+        if (!$.cookie('preview')) {
+            alert('undefined');
         } else {
             alert('preview är nu på')
             alert($.cookie('preview'));
