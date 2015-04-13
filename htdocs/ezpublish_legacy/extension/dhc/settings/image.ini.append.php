a721483ca6b876c1efc0e68dd771f256b9ed90e4
HakanBergman <hakan.bergman@drumedar.se>
2015-04-13 14:59:26 +0200

    Master

--- htdocs/ezpublish_legacy/extension/dhc/settings/image.ini.append.php
+++ htdocs/ezpublish_legacy/extension/dhc/settings/image.ini.append.php
@@ -1,12 +1,11 @@
-<?php /* #?ini charset="utf-8"?
+<?php /* 
 
 [AliasSettings]
-AliasList[]
 AliasList[]=widget_image_list_slider
 
 [widget_image_list_slider]
 Reference=
 Filters[]
-Filters[]=geometry/scaledownwidthonly=300
+Filters[]=geometry/scaledowwidthnonly=300
 
 */ ?>
