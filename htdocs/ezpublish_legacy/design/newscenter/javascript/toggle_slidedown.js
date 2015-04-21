8be8917728017de030f25a89406cb9e05e35e869
HakanBergman <hakan.bergman@drumedar.se>
2015-04-21 17:49:29 +0200

    Master

--- htdocs/ezpublish_legacy/design/newscenter/javascript/toggle_slidedown.js
+++ htdocs/ezpublish_legacy/design/newscenter/javascript/toggle_slidedown.js
@@ -4,8 +4,8 @@
         slidedown_target = $(this).attr('data-href');
         if ($('.'+$(this).attr('data-href')).css('display') == 'none') {
             $('.' + $(this).attr('data-href')).slideDown('fast');
-            $('#' + $(this).attr('id') + ' .glyphicon-menu-up').hide();
-            $('#' + $(this).attr('id') + ' .glyphicon-menu-down').show();
+            $('#' + $(this).attr('id') + ' .glyphicon-menu-down').hide();
+            $('#' + $(this).attr('id') + ' .glyphicon-menu-up').show();
         } else {
             $('.' + $(this).attr('data-href')).slideUp('fast');
             $('#' + $(this).attr('id') + ' .glyphicon-menu-down').hide();
