aa16f4acd96140a91a1e0f856d4243ab286cb231
HakanBergman <hakan.bergman@drumedar.se>
2015-04-21 17:56:13 +0200

    Master

--- htdocs/ezpublish_legacy/design/newscenter/javascript/toggle_slidedown.js
+++ htdocs/ezpublish_legacy/design/newscenter/javascript/toggle_slidedown.js
@@ -1,6 +1,8 @@
 ﻿$(document).ready(function () {
     $('.slidedown').on('click', function (e) {
         e.preventDefault();
+        alert(current_slider_id);
+        current_slider_id = $($(this)).attr('id');        
         slidedown_target = $(this).attr('data-href');
         if ($('.'+$(this).attr('data-href')).css('display') == 'none') {
             $('.' + $(this).attr('data-href')).slideDown('fast');
@@ -11,6 +13,7 @@
             $('.' + $(this).attr('data-href')).slideUp('fast');
             $('#' + $(this).attr('id') + ' .glyphicon-menu-up').hide();
             $('#' + $(this).attr('id') + ' .glyphicon-menu-down').show();
+            $($(this)).removeClass('active');
         }
     });
 });
