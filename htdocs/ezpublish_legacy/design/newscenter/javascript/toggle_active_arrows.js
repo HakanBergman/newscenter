63e8e209572ef0e64c9ab67d9b2020a340523bfb
HakanBergman <hakan.bergman@drumedar.se>
2015-04-28 16:52:50 +0200

    Master

--- htdocs/ezpublish_legacy/design/newscenter/javascript/toggle_active_arrows.js
+++ htdocs/ezpublish_legacy/design/newscenter/javascript/toggle_active_arrows.js
@@ -3,7 +3,7 @@
         if (!$(this).parent().attr('data-open')) {
             /* Slide Down */
             /* Set background on active slider */
-            setTimeout(function() { $(this).parent().addClass('bg-white'); alert("Hello"); }, 3000);
+            setTimeout(function() { $(this).parent('li').addClass('bg-white'); alert("Hello"); }, 3000);
         }
     });
 });
