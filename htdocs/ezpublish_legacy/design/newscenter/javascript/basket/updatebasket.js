cd05e0e20ce63a62c33d363e8c16df9277f21aaf
HakanBergman <hakan.bergman@drumedar.se>
2015-05-12 13:48:05 +0200

    Master

--- htdocs/ezpublish_legacy/design/newscenter/javascript/basket/updatebasket.js
+++ htdocs/ezpublish_legacy/design/newscenter/javascript/basket/updatebasket.js
@@ -3,7 +3,9 @@ $(document).ready(function () {
     $('.updatebasket').on('click', function(e) {
         e.preventDefault();
         $.post("/webshop/updatebasket", function (data) {
-            alert(data);
+            if (data == "Success") {
+                alert('Uppdateringen genomförd.');
+            }
         });
     });
 });
