b51214506eb4db994a935bec05567938ea1835aa
HakanBergman <hakan.bergman@drumedar.se>
2015-05-12 15:12:59 +0200

    Master

--- htdocs/ezpublish_legacy/extension/dhc_shop/modules/webshop/ezupdatebasket.php
+++ htdocs/ezpublish_legacy/extension/dhc_shop/modules/webshop/ezupdatebasket.php
@@ -3,10 +3,11 @@
 $itemCountList = array(intval($_POST['itemCountList']));
 $itemIDList = array(intval($_POST['itemIDList']));
 
-$operationResult = eZOperationHandler::execute('shop', 'updatebasket', array( 'item_count_list' => $itemCountList, 'item_id_list' => $itemIDList));
-$Result = array();
-$Result['pagelayout'] = '';
-$Result['content'] = 'Success';
-return $Result['content'];                                                                          
+if ($operationResult = eZOperationHandler::execute('shop', 'updatebasket', array( 'item_count_list' => $itemCountList, 'item_id_list' => $itemIDList))) {
+    $Result = array();
+    $Result['pagelayout'] = '';
+    $Result['content'] = 'Success';
+    return trim($Result['content']); 
+}
 
 ?>
