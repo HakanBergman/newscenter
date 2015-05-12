00ee566370e234bc4703f7df829938ac353ec869
HakanBergman <hakan.bergman@drumedar.se>
2015-05-12 16:04:36 +0200

    Master

--- htdocs/ezpublish_legacy/extension/dhc_shop/modules/webshop/ezupdatebasket.php
+++ htdocs/ezpublish_legacy/extension/dhc_shop/modules/webshop/ezupdatebasket.php
@@ -4,13 +4,13 @@
 $itemCountList = array(intval($_POST['itemCountList']));
 $itemIDList = array(intval($_POST['itemIDList']));
 
-if (is_numeric($itemCountList[0]) && $itemCountList[0] > 0 && is_numeric($itemIDList[0]) && $itemIDList[0] > 0 && $_SERVER['REMOTE_ADDR'] == "217.78.29.220") {
+#if (is_numeric($itemCountList[0]) && $itemCountList[0] > 0 && is_numeric($itemIDList[0]) && $itemIDList[0] > 0 && $_SERVER['REMOTE_ADDR'] == "217.78.29.220") {
     if ($operationResult = eZOperationHandler::execute('shop', 'updatebasket', array( 'item_count_list' => $itemCountList, 'item_id_list' => $itemIDList))) {
         $Result = array();
         $Result['pagelayout'] = '';
         $Result['content'] = 'Success';
         return trim($Result['content']); 
     }
-}
+#}
 
 ?>
