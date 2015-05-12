d35f7b99bb42733a2160eaafd8a69e947e58d648
HakanBergman <hakan.bergman@drumedar.se>
2015-05-12 13:08:42 +0200

    Master

--- htdocs/ezpublish_legacy/extension/dhc_shop/settings/site.ini.append.php
+++ htdocs/ezpublish_legacy/extension/dhc_shop/settings/site.ini.append.php
@@ -1,5 +1,13 @@
 <?php /* #?ini charset="utf-8"?
 
+# Permissions to the specific ajax calls are handled inside the ezjscore/call
+# view, so only comment this out if you want to disable it!
+[RoleSettings]
+PolicyOmitList[]=ezjscore/updatebasket
+
+[SiteAccessSettings]
+AnonymousAccessList[]=ezjscore/updatebasket
+
 [TemplateSettings]
 ExtensionAutoloadPath[]=dhc_shop
 
