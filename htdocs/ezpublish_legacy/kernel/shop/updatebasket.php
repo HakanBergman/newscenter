5cbf6240b7d3defb3ec0c4e85d7d48b8dcf6413f
HakanBergman <hakan.bergman@drumedar.se>
2015-05-12 12:44:45 +0200

    Master

--- htdocs/ezpublish_legacy/kernel/shop/updatebasket.php
+++ htdocs/ezpublish_legacy/kernel/shop/updatebasket.php
@@ -15,7 +15,7 @@ $itemIDList = $http->sessionVariable( 'ProductItemIDList' );
 
 $operationResult = eZOperationHandler::execute( 'shop', 'updatebasket', array( 'item_count_list' => $itemCountList,
                                                                                'item_id_list' => $itemIDList ) );
-
+print_r($operationResult);
 switch( $operationResult['status'] )
 {
     case eZModuleOperationInfo::STATUS_HALTED:
