365cbd30507435b57f488ed21e165ace08cddaaa
HakanBergman <hakan.bergman@drumedar.se>
2015-05-12 14:39:49 +0200

    Master

--- htdocs/ezpublish_legacy/extension/dhc_shop/modules/webshop/ezupdatebasket.php
+++ htdocs/ezpublish_legacy/extension/dhc_shop/modules/webshop/ezupdatebasket.php
@@ -29,10 +29,13 @@ if (!$error) {
     $http->setSessionVariable( 'ProductItemIDList', $itemIDList );
     $operationResult = eZOperationHandler::execute( 'shop', 'updatebasket', array( 'item_count_list' => $itemCountList,
                                                                                    'item_id_list' => $itemIDList ) );
+                                                                                   
+    print_r($http);
     
     $Result = array();
     $Result['pagelayout'] = '';
     $Result['content'] = $itemIDList;
+    
     
 } else {
     $Result = array();
