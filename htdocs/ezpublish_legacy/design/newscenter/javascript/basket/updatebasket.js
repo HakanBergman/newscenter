d941effff947fa3de4ef732d943726e3990b0f56
HakanBergman <hakan.bergman@drumedar.se>
2015-05-12 13:38:12 +0200

    Master

--- htdocs/ezpublish_legacy/design/newscenter/javascript/basket/updatebasket.js
+++ htdocs/ezpublish_legacy/design/newscenter/javascript/basket/updatebasket.js
@@ -1,10 +1,9 @@
 ﻿/* Make the Menu bar toggle */
 $(document).ready(function () {
     $('.updatebasket').on('click', function(e) {
-        alert('test');
         e.preventDefault();
         $.post("/webshop/updatebasket", function (data) {
-            alert("Data Loaded: " + data);
+            alert(data);
         });
     });
 });
--- htdocs/ezpublish_legacy/extension/dhc_shop/modules/webshop/ezupdatebasket.php
+++ htdocs/ezpublish_legacy/extension/dhc_shop/modules/webshop/ezupdatebasket.php
@@ -34,15 +34,12 @@ $itemCountList = $http->sessionVariable( 'ProductItemCountList' );
 $itemIDList = $http->sessionVariable( 'ProductItemIDList' );
 
 $operationResult = eZOperationHandler::execute( 'shop', 'updatebasket', array( 'item_count_list' => $itemCountList,
-                                                                               'item_id_list' => $itemIDList ) );
+                                                                               'item_id_list' => $itemIDList ) )
 
 $Result = array();
 $Result['pagelayout'] = '';
-$Result['content'] = 'Hello World!';
-                                                                               
-eZDB::checkTransactionCounter();
-eZExecution::cleanExit();
-                                                                               
-#$module->redirectTo( '/shop/basket/' );
+$Result['content'] = 'Completed';
+
+return $Result['content'];                                                                               
 
 ?>
