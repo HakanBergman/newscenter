a6e4dcf3a2de08b7aacb3ddfaa60dd15d9fccf1f
HakanBergman <hakan.bergman@drumedar.se>
2015-05-12 14:18:01 +0200

    Master

--- htdocs/ezpublish_legacy/design/newscenter/javascript/basket/updatebasket.js
+++ htdocs/ezpublish_legacy/design/newscenter/javascript/basket/updatebasket.js
@@ -3,8 +3,7 @@ $(document).ready(function () {
     $('.updatebasket').on('click', function(e) {
         e.preventDefault();
         var product_id = $(this).attr('product-id');
-        alert($(this).attr('product-id'));
-        alert($('#product_number_' + id).val());
+        alert($('#product_number_' + product_id).val());
         $.post("/webshop/updatebasket", { itemCountList: 3, itemIDList: product_id }, function (data) {
             if (data == "Success") {
                 alert('Uppdateringen genomförd.');
