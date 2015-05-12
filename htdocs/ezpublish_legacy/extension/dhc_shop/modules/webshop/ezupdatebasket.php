35c69edaf1413338525d0cf235f94e8584b2b76e
HakanBergman <hakan.bergman@drumedar.se>
2015-05-12 13:21:14 +0200

    Master

--- htdocs/ezpublish_legacy/extension/dhc_shop/modules/webshop/ezupdatebasket.php
+++ htdocs/ezpublish_legacy/extension/dhc_shop/modules/webshop/ezupdatebasket.php
@@ -16,6 +16,6 @@ $itemIDList = $http->sessionVariable( 'ProductItemIDList' );
 $operationResult = eZOperationHandler::execute( 'shop', 'updatebasket', array( 'item_count_list' => $itemCountList,
                                                                                'item_id_list' => $itemIDList ) );
                                                                                
-$module->redirectTo( '/shop/basket/' );
+#$module->redirectTo( '/shop/basket/' );
 
 ?>
