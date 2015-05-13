dcfd6b17fee6beb6bb4a0dd3298ac6fb13dc20cd
HakanBergman <hakan.bergman@drumedar.se>
2015-05-13 14:26:00 +0200

    Master

--- htdocs/ezpublish_legacy/extension/dhc_shop/modules/webshop/ezaddtobasket.php
+++ htdocs/ezpublish_legacy/extension/dhc_shop/modules/webshop/ezaddtobasket.php
@@ -24,7 +24,7 @@ if (!$error) {
                                                                                   'object_id' => $ObjectID,
                                                                                   'quantity' => $quantity,
                                                                                   'option_list' => $OptionList));
-    $Result['content'] =  'Success';
+    $Result['content'] =  trim('Success');
 } else {
     $Result['content'] = null;
 }
