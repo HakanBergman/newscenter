2d6adecc2d64608cd3b6b837110dc4a47e90c00e
HakanBergman <hakan.bergman@drumedar.se>
2015-05-13 12:32:47 +0200

    Master

--- htdocs/ezpublish_legacy/extension/dhc_shop/modules/webshop/ezaddtobasket.php
+++ htdocs/ezpublish_legacy/extension/dhc_shop/modules/webshop/ezaddtobasket.php
@@ -8,12 +8,15 @@ if (!is_numeric($quantity) or $quantity <= 0 ) { $quantity = 1; }
 if (!is_numeric($ObjectID)) { $error = true; }
 if (!eZContentObject::exists($ObjectID)) { $error = true; }
 
-/*
-$object = eZContentObject::fetch( $ObjectID );
-if (!$object->canRead()) { $error = true; }
 
-$error = $basket->canAddProduct( $object );
-if ($error !== eZError::SHOP_OK) { $error = true; }
+$object = eZContentObject::fetch($ObjectID);
+if (!$object->canRead()) { $error = true; }
+if (!$error) {
+    $error = $basket->canAddProduct($object);
+    if ($error !== eZError::SHOP_OK) { $error = true; }
+}
+
+/*
 
 $OptionList = $http->sessionVariable( "AddToBasket_OptionList_" . $ObjectID);
 $operationResult = eZOperationHandler::execute( 'shop', 'addtobasket', array( 'basket_id' => $basket->attribute('id'),
