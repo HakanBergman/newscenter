747469c3474b74a56ada59012987797e38329d5b
HakanBergman <hakan.bergman@drumedar.se>
2015-05-13 12:57:21 +0200

    Master

--- htdocs/ezpublish_legacy/design/newscenter/javascript/basket/addtobasket.js
+++ htdocs/ezpublish_legacy/design/newscenter/javascript/basket/addtobasket.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         e.preventDefault();
         var product_id = $(this).attr('product-id');
         var product_price = $(this).attr('product-price');
-        var quantity = $('.form_number_' + product_id).html();
+        var quantity = $('.form_number_' + product_id + ' .quantity_number_' + product_id).html();
         alert(quantity);
         $('.cart_total_items').html(parseInt(current_items) + 1);
         $.post("/webshop/addtobasket", { quantity: product_count, objectid: product_id }, function (data) {
