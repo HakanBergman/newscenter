cedb220bee42f8d82ea99e7c69cd401fc5864649
HakanBergman <hakan.bergman@drumedar.se>
2015-05-13 12:41:17 +0200

    Master

--- htdocs/ezpublish_legacy/extension/dhc_shop/modules/webshop/ezaddtobasket.php
+++ htdocs/ezpublish_legacy/extension/dhc_shop/modules/webshop/ezaddtobasket.php
@@ -17,7 +17,12 @@ if (!$error) {
     if ($error !== eZError::SHOP_OK) { $error = true; }
 }
 
+
 $OptionList = $http->sessionVariable( "AddToBasket_OptionList_" . $ObjectID);
+print_r($basket);
+print_r($ObjectID);
+print_r($quantity);
+print_r($OptionList);
 $operationResult = eZOperationHandler::execute( 'shop', 'addtobasket', array( 'basket_id' => $basket->attribute('id'),
                                                                               'object_id' => $ObjectID,
                                                                               'quantity' => $quantity,
