20e7bcb7f2c14d179516923237233f7c8794b7fe
HakanBergman <hakan.bergman@drumedar.se>
2015-05-18 11:29:58 +0200

    Master

--- htdocs/ezpublish_legacy/extension/dhc_shop/modules/webshop/ezaddtobasket.php
+++ htdocs/ezpublish_legacy/extension/dhc_shop/modules/webshop/ezaddtobasket.php
@@ -4,7 +4,7 @@ $http = eZHTTPTool::instance();
 $basket = eZBasket::currentBasket();
 
 $quantity = $_POST['quantity'];
-$ObjectID = 331;
+$ObjectID = 3311;
 if (!is_numeric($quantity) or $quantity <= 0 ) { $quantity = 1; }
 if (!is_numeric($ObjectID)) { $error = true; }
 if (!eZContentObject::exists($ObjectID)) { $error = true; }
