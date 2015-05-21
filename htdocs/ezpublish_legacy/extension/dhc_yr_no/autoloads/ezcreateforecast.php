0fb33c8382020f7c3fb406f148174594591a0a20
HakanBergman <hakan.bergman@drumedar.se>
2015-05-21 20:57:56 +0200

    Master

--- htdocs/ezpublish_legacy/extension/dhc_yr_no/autoloads/ezcreateforecast.php
+++ htdocs/ezpublish_legacy/extension/dhc_yr_no/autoloads/ezcreateforecast.php
@@ -28,7 +28,14 @@ class eZCreateForecast
         {
             case 'ezcreateforecast':
             {   
-                echo "This will send an email";
+                #require_once __DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."autoload.php";
+                #$yr = Yr\Yr::create("Norway/Oslo/Oslo/Oslo", "/tmp");
+                #$forecast = $yr->getCurrentForecast();
+                #printf("Time: %s to %s\n", $forecast->getFrom()->format("H:i"), $forecast->getTo()->format("H:i"));
+                #printf("Temp: %s %s \n", $forecast->getTemperature(), $forecast->getTemperature('unit'));
+                #printf("Wind: %smps %s\n", $forecast->getWindSpeed(), $forecast->getWindDirection('name'));            
+                #echo "This will send an email";
+                echo dirname(__FILE__);
                 #$operatorValue = $_COOKIE["captcha"];                
             } break;
         }
