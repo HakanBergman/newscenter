edd9fdadf9a32e39195630cb42e991f6fd1a9f15
HakanBergman <hakan.bergman@drumedar.se>
2015-05-21 21:02:44 +0200

    Master

--- htdocs/ezpublish_legacy/extension/dhc_yr_no/autoloads/ezcreateforecast.php
+++ htdocs/ezpublish_legacy/extension/dhc_yr_no/autoloads/ezcreateforecast.php
@@ -28,16 +28,13 @@ class eZCreateForecast
         {
             case 'ezcreateforecast':
             {   
-                #require_once __DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."autoload.php";
-                #$yr = Yr\Yr::create("Norway/Oslo/Oslo/Oslo", "/tmp");
-                #$forecast = $yr->getCurrentForecast();
-                #printf("Time: %s to %s\n", $forecast->getFrom()->format("H:i"), $forecast->getTo()->format("H:i"));
-                #printf("Temp: %s %s \n", $forecast->getTemperature(), $forecast->getTemperature('unit'));
-                #printf("Wind: %smps %s\n", $forecast->getWindSpeed(), $forecast->getWindDirection('name'));            
-                #echo "This will send an email";
                 $extension_folder = str_replace('autoloads', '', dirname(__FILE__));
                 require_once $extension_folder.'api/autoload.php';
-  
+                $yr = Yr\Yr::create("Norway/Oslo/Oslo/Oslo", "/tmp");
+                $forecast = $yr->getCurrentForecast();
+                printf("Time: %s to %s\n", $forecast->getFrom()->format("H:i"), $forecast->getTo()->format("H:i"));
+                printf("Temp: %s %s \n", $forecast->getTemperature(), $forecast->getTemperature('unit'));
+                printf("Wind: %smps %s\n", $forecast->getWindSpeed(), $forecast->getWindDirection('name'));              
                 #$operatorValue = $_COOKIE["captcha"];                
             } break;
         }
