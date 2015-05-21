fd1f8081a83dd129e5873eb2cbcbdd8488600647
HakanBergman <hakan.bergman@drumedar.se>
2015-05-21 22:28:57 +0200

    Master

--- htdocs/ezpublish_legacy/extension/dhc_yr_no/autoloads/ezcreateforecast.php
+++ htdocs/ezpublish_legacy/extension/dhc_yr_no/autoloads/ezcreateforecast.php
@@ -28,10 +28,14 @@ class eZCreateForecast
         {
             case 'ezcreateforecast':
             {   
+            
+              /* Fetch Weather Information from JSON API */
               $json = file_get_contents("http://api.openweathermap.org/data/2.5/weather?lat=57.723152&lon=11.767669");
               $json_decode = json_decode($json);
-              print_r($json_decode->weather);
-              var_dump(json_decode($json));
+              
+              /* Temperature */
+              $temperature['low'] = $json_decode->main->temp_min;
+              print_r($temperature);
             } break;
         }
     }
