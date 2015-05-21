2410db7fb5e1e01d888ada4d5a1dc96ed6626ab8
HakanBergman <hakan.bergman@drumedar.se>
2015-05-21 22:04:02 +0200

    Master

--- htdocs/ezpublish_legacy/extension/dhc_yr_no/autoloads/ezcreateforecast.php
+++ htdocs/ezpublish_legacy/extension/dhc_yr_no/autoloads/ezcreateforecast.php
@@ -28,16 +28,8 @@ class eZCreateForecast
         {
             case 'ezcreateforecast':
             {   
-                $extension_folder = str_replace('autoloads', '', dirname(__FILE__));
-                require_once $extension_folder.'api/autoload.php';
-                require_once $extension_folder.'api/yr.php';
-                #$yr = Yr\Yr::create("Norway/Oslo/Oslo/Oslo", "/tmp");
-                #$forecast = $yr->getCurrentForecast();
-                #printf("Time: %s to %s\n", $forecast->getFrom()->format("H:i"), $forecast->getTo()->format("H:i"));
-                #printf("Temp: %s %s \n", $forecast->getTemperature(), $forecast->getTemperature('unit'));
-                #printf("Wind: %smps %s\n", $forecast->getWindSpeed(), $forecast->getWindDirection('name'));              
-                #printf("Pressure: %smps %s\n", $forecast->getPressure(), $forecast->getPressure('value'));  
-                #$operatorValue = $_COOKIE["captcha"];                
+              #$json = file_get_contents("http://api.openweathermap.org/data/2.5/weather?lat=57.723152&lon=11.767669");
+              #var_dump(json_decode($json));
             } break;
         }
     }
